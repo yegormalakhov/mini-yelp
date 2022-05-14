03697f9850fdaebf81121975441016244525ad3f
Kyle Phillips <justinkphillips@gmail.com>
2022-05-14 16:12:44 +0200

    did the best we could

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import Footer from './Components/Footer';
 import Filters from './Components/Filters';
 import Restaurant from './Components/Restaurant';
 import RestaurantsList from './Components/RestaurantsList';
-import {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react';
+import { NavLink, Routes, Route, useParams } from "react-router-dom";
+
 
 
 function App() {
   const [priceCategory, setPriceCategory] = useState();
   const [ratingCategory, setRatingCategory] = useState();
   const [wordQuery, setWordQuery] = useState("");
+  const [restaurants, setRestaurants] = useState();
+  const [filteredRestaurants, setFilteredRestaurants] = useState();
+
+  //  const { restaurantId } = useParams();
+  //  const singleRestaurant = restaurants.filter((restaurant) => restaurantId ? restaurants._id == restaurantId : restaurants);
 
   const handlePriceFilter = (event) =>{
-    // console.log(event.target.innerText);
-    const price = event.target.innerText;
+    const price = event.target.innerText.length;
     setPriceCategory(price);
+    
+
   }
 
   const handleRatingFilter = (event) => {
-    // console.log(event.target.innerText);
     const rating = event.target.innerText;
     setRatingCategory(rating);
 
@@ -30,8 +37,6 @@ function App() {
   };
 
   useEffect(()=>{
-    // console.log(ratingCategory);
-    // console.log(priceCategory);
 
     fetch("https://miniyelpback.herokuapp.com/restaurants/")
       .then((response) => {
@@ -43,19 +48,35 @@ function App() {
         return response.json();
       })
       .then((data) => {
-        console.log(`this is ${data}`);
+        setRestaurants(data);
       })
       .catch((error) => {
         console.log(error);
       });
+
+      if(priceCategory){
+        const targetedCategory = restaurants.filter((restaurant)=> restaurant.price_category === priceCategory);
+        console.log(targetedCategory);
+        setFilteredRestaurants(targetedCategory);
+      }
+
+      // if(ratingCategory){
+      //   const targetedCategory = restaurants.filter((restaurant)=> restaurant.star_rating === ratingCategory);
+      //   setRestaurants(targetedCategory);
+      // }
   },[priceCategory, ratingCategory, wordQuery]);
   
   return (
     <div>
       <Header />
       <div className='Content'>
+        {/* <Routes>
+          <Route path="/restaurants"  element={<RestaurantsList props={restaurants} searchWord={handleSearchWord}/>} />
+          <Route path=`/restaurants/${restaurantId}`  element={<Restaurant props={singleRestaurant} />} />
+        </Routes> */}
         <Filters onPriceClick={handlePriceFilter} onRatingClick={handleRatingFilter} />
-        <RestaurantsList searchWord={handleSearchWord} />
+        { restaurants && <RestaurantsList props={filteredRestaurants ? filteredRestaurants : restaurants} searchWord={handleSearchWord} /> }
+        {/* { filteredRestaurants && <RestaurantsList props={filteredRestaurants} searchWord={handleSearchWord} /> } */}
       </div>
       
       <Footer />
